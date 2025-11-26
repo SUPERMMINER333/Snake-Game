9379ecebaa5b6350c61b6f109a46b7a798ab2844
SUPERMMINER <nrho333@gmail.com>
2025-11-26 15:29:33 +0100

    Revert "v1 Powerup"
    This reverts commit fd0e7d5a98a15797a3ea0f88dddd711db4bcae54.

--- Snake Game/2.Game.cs
+++ Snake Game/2.Game.cs
@@ -337,25 +337,7 @@ namespace Snake_Game
             Setting.Speed = 100;
             gameTime.Interval = Setting.Speed;
 
-            powerup = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
-        }
-
-        private void EatPowerup()
-        {
-            Circle body = new Circle
-            {
-                X = Snake[Snake.Count - 1].X,
-                Y = Snake[Snake.Count - 1].Y
-            };
-            
-            Setting.Speed = 100;
-            gameTime.Interval = Setting.Speed;
-
-            if (Snake.Count > 1)
-            {
-                Snake.RemoveAt(Snake.Count - 1);
-                Snake.RemoveAt(Snake.Count - 1);
-            }
+            Snake.Remove(body);
 
             powerup = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
         }
